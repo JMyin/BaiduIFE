5e282421a42717f5a1ddc764ef6cf2298b5ba8e0
username <hcwcyz@gmail.com>
2016-04-04 23:32:17 +0800

    task-23

--- task_23/task_23.js
+++ task_23/task_23.js
@@ -1,3 +1,33 @@
-/**
- * Created by tongqi on 4/1/16.
- */
+function $(id)
+{
+    return document.getElementById(id);
+}
+
+function drawArc()
+{
+    var canvas=$('canvas');
+    var cans=canvas.getContext('2d');
+    cans.arc(400,300,100,0,Math.PI*2);
+    cans.fillStyle = 'blue';
+    cans.fill();
+}
+
+function drawPlane(num)
+{
+
+}
+
+
+drawArc();
+
+function addEvent(element,type,handler){
+    if(element.addEventListener) {
+        element.addEventListener(type,handler,false);
+    }
+    else if(element.attachEvent)
+    {
+        element.attachEvent("on"+type,handler                          );
+    }
+    else
+        element["on" + type]=handler;
+}
